2743f62383944d69b5c7fc1c9ff70f595883602c
Casey Doig <cddoigkn@gmail.com>
2023-12-08 17:03:23 -0600

    adding projects to portfolio, along with carousel

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,40 @@
 import { Container, Row, Col } from 'react-bootstrap'
+import Carousel from 'react-bootstrap/Carousel';
+import Halfofsvg from '../../public/img/halfofsvg';
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+
 
 export default function Portfolio() {
   return (
     <Container fluid>
       <Row className='justify-content-between align-items-center'>
         <Col>
-         <p>Projects go here</p>
+          <Carousel>
+            <Carousel.Item>
+              < Halfofsvg text="First slide" />
+              <Carousel.Caption>
+                <h3>My Work with SVG</h3>
+                {/* <img src="./img/halfofsvg.png" /> */}
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <ExampleCarouselImage text="Second slide" />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <ExampleCarouselImage text="Third slide" />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </Col>
       </Row>
     </Container>
